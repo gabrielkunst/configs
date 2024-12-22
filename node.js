4fb1b75c7d5953cd94a942f39cb9355b9451dc2f
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-12-21 23:48:41 -0300

    chore: add configs

---
+++ node.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'es5',
+        arrowParens: 'always',
+        semi: false,
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
